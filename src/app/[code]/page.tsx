f111de32608106939ee1b26660f1ea6a0dda214b
Jeremias Villane <vil.jeremias@gmail.com>
2023-11-03 02:47:23 -0300

    new loading redirect screen

--- src/app/[code]/page.tsx
+++ src/app/[code]/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { getOriginalUrl } from "@/server-actions";
+import { Spinner } from "@nextui-org/react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
+import SnipperLogo from "../../../public/snipper.png";
 
 interface Params {
   code: string;
@@ -32,4 +35,17 @@ export default function ({ params }: { params: Params }) {
       };
     }
   }, []);
+
+  return (
+    <div className="w-screen h-screen fixed inset-0 flex flex-col items-center justify-center z-[333] bg-slate-200 dark:bg-[#0f172a]">
+      <Image
+        src={SnipperLogo}
+        alt="Snipper"
+        width={99}
+        height={99}
+        className="dark:invert mb-8"
+      />
+      <Spinner size="lg" />
+    </div>
+  );
 }
