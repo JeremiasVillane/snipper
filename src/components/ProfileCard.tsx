f98cf58ad8a914e00cf0a4df3e607a64689ef694
Jeremias Villane <vil.jeremias@gmail.com>
2023-11-04 16:31:01 -0300

    showing total clicks in ProfileCard

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,9 +1,12 @@
 import SignOutButton from "@/components/SignOutButton";
+import getTotalClicks from "@/server-actions/get-total-clicks";
 import { Avatar } from "@nextui-org/react";
 import { currentUser } from "next-auth";
 import NextLink from "next/link";
 
-export default function ProfileCard({ user }: { user: currentUser }) {
+export default async function ProfileCard({ user }: { user: currentUser }) {
+  const totalClicks = await getTotalClicks();
+
   return (
     <div
       className="rounded-2xl flex p-6 flex-col space-y-5 bg-gradient-to-br from-white to-slate-300/[0.2] dark:from-slate-800 dark:to-slate-800/[0.2] border border-transparent hover:border-slate-300 dark:hover:border-slate-700"
@@ -30,7 +33,7 @@ export default function ProfileCard({ user }: { user: currentUser }) {
           <p className="text-xs">Links</p>
         </NextLink>
         <div>
-          <p className="text-green-500 font-bold">468</p>
+          <p className="text-green-500 font-bold">{totalClicks}</p>
           <p className="text-xs">Visits</p>
         </div>
       </div>
