f9d89b8db86c7b54b32de6c8942a8478f485dc3f
Jeremias Villane <vil.jeremias@gmail.com>
2023-11-03 22:29:10 -0300

    disabled landing wallpaper on smaller devices

--- src/components/ui/BoxesContainer.tsx
+++ src/components/ui/BoxesContainer.tsx
@@ -4,7 +4,7 @@ import { useMediaQuery } from "@/hooks";
 import { motion } from "framer-motion";
 
 export default function BoxesContainer(): JSX.Element {
-  const isSmallerThan1080 = useMediaQuery("(max-width: 1080px)");
+  const isGreaterThan1080 = useMediaQuery("(min-width: 1080px)");
   const rows = new Array(150).fill(1);
   const cols = new Array(100).fill(1);
   let colors = [
@@ -29,43 +29,47 @@ export default function BoxesContainer(): JSX.Element {
       }}
       className="absolute left-1/4 p-4 -top-1/4 flex  -translate-x-1/2 -translate-y-1/2 w-full h-full z-0"
     >
-      {rows.map((_, i) => (
-        <motion.div
-          key={`row` + i}
-          className="w-16 h-8  border-l  border-slate-700 relative"
-        >
-          {cols.map((_, j) => (
-            <motion.div
-              whileHover={{
-                backgroundColor: isSmallerThan1080 ? "" : getRandomColor(),
-                transition: { duration: 0 },
-              }}
-              animate={{
-                transition: { duration: 2 },
-              }}
-              key={`col` + j}
-              className="w-16 h-8  border-r border-t border-slate-700 relative"
-            >
-              {j % 2 === 0 && i % 2 === 0 ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="absolute h-6 w-10 -top-[14px] -left-[22px] text-slate-700 stroke-[1px] pointer-events-none"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v12m6-6H6"
-                  />
-                </svg>
-              ) : null}
-            </motion.div>
-          ))}
-        </motion.div>
-      ))}
+      {isGreaterThan1080 ? (
+        rows.map((_, i) => (
+          <motion.div
+            key={`row` + i}
+            className="w-16 h-8  border-l  border-slate-700 relative"
+          >
+            {cols.map((_, j) => (
+              <motion.div
+                whileHover={{
+                  backgroundColor: getRandomColor(),
+                  transition: { duration: 0 },
+                }}
+                animate={{
+                  transition: { duration: 2 },
+                }}
+                key={`col` + j}
+                className="w-16 h-8  border-r border-t border-slate-700 relative"
+              >
+                {j % 2 === 0 && i % 2 === 0 ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="absolute h-6 w-10 -top-[14px] -left-[22px] text-slate-700 stroke-[1px] pointer-events-none"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 6v12m6-6H6"
+                    />
+                  </svg>
+                ) : null}
+              </motion.div>
+            ))}
+          </motion.div>
+        ))
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
